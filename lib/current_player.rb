281a9d1aae9bbb528e25d55b4345232192c79573
Leonard Chew <leochew@gmail.com>
2017-09-04 08:43:27 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,8 +3,11 @@ counter = 0
 board.each do |index|
   if index == "X" ||
     index == "O"
+    #if the index is occupied either X or O
     counter += 1
   end
 end
 counter
 end
+
+def current_player
